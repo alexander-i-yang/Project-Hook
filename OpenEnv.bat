adcef76fb3e94e8af8958d2db52f0fea9be76713
Alex Yang <alex@yang2k.com>
2024-01-15 22:34:33 -0500

    Open workspace automation

---
+++ OpenEnv.bat
@@ -0,0 +1,4 @@
+ECHO Starting Unity Project...
+START "Open Rider" "rider64.exe" C:\Users\uncle\Unity\Project-Hook
+START "Open Unity" "C:\Program Files\Unity\Hub\Editor\2022.3.14f1\Editor\Unity.exe" -projectPath "C:\Users\uncle\Unity\Project-Hook"
+START "Open Aseprite" "C:\Program Files (x86)\Steam\steamapps\common\Aseprite\Aseprite.exe"
