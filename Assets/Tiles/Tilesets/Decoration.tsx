1fc43c4f200d682a5c394a8b6815c617da1b5e86
Alex Yang <alex@yang2k.com>
2024-01-23 12:55:46 -0500

    Alex Room 1, Amazing Decoration art

--- Assets/Tiles/Tilesets/Decoration.tsx
+++ Assets/Tiles/Tilesets/Decoration.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Decoration" tilewidth="37" tileheight="72" tilecount="5" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="Decoration" tilewidth="44" tileheight="32" tilecount="8" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="Potted Mushroom">
   <image width="9" height="13" source="../../Sprites/Decor/Mushroom.png"/>
@@ -13,7 +13,16 @@
  <tile id="3" type="Desk Lamp">
   <image width="11" height="14" source="../../Sprites/Decor/Desk Lamp.png"/>
  </tile>
- <tile id="4">
-  <image width="37" height="72" source="../../Sprites/Decor/Window.png"/>
+ <tile id="5">
+  <image width="8" height="12" source="../../Sprites/Decor/small_plant.png"/>
+ </tile>
+ <tile id="6">
+  <image width="44" height="32" source="../../Sprites/Decor/Whiteboard.png"/>
+ </tile>
+ <tile id="7">
+  <image width="8" height="12" source="../../Sprites/Decor/trash_can.png"/>
+ </tile>
+ <tile id="8">
+  <image width="24" height="32" source="../../Sprites/Decor/big_plant.png"/>
  </tile>
 </tileset>
