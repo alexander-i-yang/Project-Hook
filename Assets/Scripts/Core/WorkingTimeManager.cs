40516d62dcfa33b3f51d315350043a4ca64b5d9e
PieBob <PieBob851@gmail.com>
2024-02-09 15:49:47 -0500

    Adjusted WorkingTimeManager to update based on a elapsed time & added animation timeScaling to it.

--- Assets/Scripts/Core/WorkingTimeManager.cs
+++ Assets/Scripts/Core/WorkingTimeManager.cs
@@ -9,6 +9,15 @@ namespace Core
     {
         private float _currentTimeScale = 1;
         [SerializeField] private float timeAcceleration = 0.45f;
+        private Animator _animator;
+        private float _previousTime;
+
+
+        //private void Awake()
+        //{
+        //    _animator = GetComponent<Animator>();
+        //    _previousTime = UnityEngine.Time.time;
+        //}
 
         /*
 		 * Smooths timescale transition between different states.
@@ -17,12 +26,24 @@ namespace Core
 		 */
         public override float GetTimeScale()
         {
-            float targetTimeScale = base.GetTimeScale();
-            float scaleFactor = timeAcceleration * UnityEngine.Time.deltaTime;
-
-            _currentTimeScale = Mathf.MoveTowards(_currentTimeScale, targetTimeScale, _currentTimeScale * scaleFactor);
+            updateTimeScale();
 
             return _currentTimeScale;
+        }
+
+        /*
+		 * Updates the timeScale in a smooth manner using the time since the last update and timeAcceleration
+		 *
+		 */
+        private void updateTimeScale()
+        {
+            float timeDifference = UnityEngine.Time.time - _previousTime;
+            _previousTime = UnityEngine.Time.time;
+
+            float targetTimeScale = base.GetTimeScale();
+            float scaleFactor = timeAcceleration * timeDifference;
+            _currentTimeScale = Mathf.MoveTowards(_currentTimeScale, targetTimeScale, _currentTimeScale * scaleFactor);
+            _animator.speed = _currentTimeScale;
         }
     }
 }
