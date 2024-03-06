58e61c81e7c41589f8c39db6be7824d8fea49c0a
PieBob <PieBob851@gmail.com>
2024-03-06 13:40:57 -0500

    Tentative commit testing timescale jumping to 1

--- Assets/Scripts/Core/WorkingTimeManager.cs
+++ Assets/Scripts/Core/WorkingTimeManager.cs
@@ -31,18 +31,14 @@ namespace Core
 		 */
         private void updateTimeScale()
         {
-            if (_currentTimeScale < 0)
-            {
-                Debug.Log("Negative timescale; this shouldn't happen. The TimeScale was reset to 1 to prevent further erroring. Please let Carter/PieBob know any details to potentially fix!");
-                _currentTimeScale = 1;
-                return;
-            }
-
             float timeDifference = UnityEngine.Time.time - _previousTime;
             _previousTime = UnityEngine.Time.time;
 
             float targetTimeScale = base.GetTimeScale();
-            Debug.Log("Current timescale: " + _currentTimeScale + "\nTarget timescale: " + targetTimeScale);
+            if (targetTimeScale == 1)
+            {
+                _currentTimeScale = 1;
+            }
             float scaleFactor = (float) Math.Pow(timeAcceleration, timeDifference) - 1;
             _currentTimeScale = Mathf.MoveTowards(_currentTimeScale, targetTimeScale, _currentTimeScale * scaleFactor);
         }
