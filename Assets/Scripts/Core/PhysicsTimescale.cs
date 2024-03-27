28aa1c45a123661f51a8e1dc0ad4e3fd8bfd4057
PieBob <PieBob851@gmail.com>
2024-03-26 21:02:18 -0400

    Fixed by making it use physics2D instead of physics, oops

--- Assets/Scripts/Core/PhysicsTimescale.cs
+++ Assets/Scripts/Core/PhysicsTimescale.cs
@@ -7,26 +7,29 @@ namespace Core
     public class PhysicsTimescale : MonoBehaviour
     {
         private float _timer = 0;
+		private float _physTimePassed = 0;
 		
 		private void Awake() {
-			Physics.simulationMode = SimulationMode.Script;
+			Physics2D.simulationMode = SimulationMode2D.Script;
 		}
 		
 		private void OnEnable() {
-			Physics.simulationMode = SimulationMode.Script;
+			Physics2D.simulationMode = SimulationMode2D.Script;
 		}
 		
 		private void OnDisable() {
-			Physics.simulationMode = SimulationMode.FixedUpdate;
+			Physics2D.simulationMode = SimulationMode2D.FixedUpdate;
 		}
 		
-		private void Update() {
+		private void FixedUpdate() {
 			_timer += Game.TimeManager.DeltaTime;
 			// Unity recommends simulating physics in intervals of Time.fixedDeltaTime for stability.
 			while (_timer >= Time.fixedDeltaTime)
 			{
 				_timer -= Time.fixedDeltaTime;
-                Physics.Simulate(Time.fixedDeltaTime);
+				_physTimePassed += Time.fixedDeltaTime;
+				Debug.Log(_physTimePassed);
+                Physics2D.Simulate(Time.fixedDeltaTime);
 			}
 		}
     }
